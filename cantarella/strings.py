b871798a67d2040460036df76095947d0c496fb2
teamreelsmart <team.reelsmart@gmail.com>
2026-03-15 18:22:38 +0530

    Update strings.py

--- cantarella/strings.py
+++ cantarella/strings.py
@@ -55,7 +55,7 @@ HELP_TXT = """<b>🚀 Welcome to cantarella Save Restricted Bot - Complete Guide
 <b>💡 Tips</b>
 • Free users: 10 saves/day + 5 files per batch
 • Premium users: Unlimited saves & batch size
-• Contact @cantarellabots for support or premium purchase
+• Contact @SnapLoverXBot for support or premium purchase
 
 <b>Thank you for using cantarella! ❤️</b>
 """
@@ -105,7 +105,7 @@ COMMANDS_TXT = """<b>📜 All Available Commands</b>
 </blockquote>
 
 <b>🔥 Premium = Unlimited Everything</b>
-<i>Contact @cantarellabots to upgrade!</i>
+<i>Contact @SnapLoverXBot to upgrade!</i>
 """
 
 # Dont remove Credits
