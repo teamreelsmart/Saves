4c3b16f15cb7798fc458c9a9c3667e647b958ffc
teamreelsmart <team.reelsmart@gmail.com>
2026-03-19 14:51:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,7 +94,7 @@ class Bot(Client):
             f"<b>Bot:</b> @{me.username}\n"
             f"<b>Users:</b> <code>{user_count} / 200</code>\n"
             f"<b>Time:</b> <code>{now.strftime('%I:%M %p')} IST</code>\n\n"
-            f"<b>Developed by @cantarellabots</b>"
+            f"<b>Developed by @TheOrviX</b>"
         )
 
         try:
